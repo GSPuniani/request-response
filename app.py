08f29cf73d12e92d75f8c199b83d0570e7abe58a
Gobind Puniani <gobindpuniani@gmail.com>
2020-10-29 02:32:39 -0700

    Completed Hello, World Tutorial

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
+
+# Import the flask library
+from flask import Flask
+
+# Set up the `app` variable to start writing routes
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """Shows a greeting to the user."""
+    return "Are you there, world? It's me, Ducky!"
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
